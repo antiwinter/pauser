d7ba73c0f9ce7cf928e25b5c4092081897827983
antiwinter <antiwinter@126.com>
2026-05-13 18:05:45 +0800

    remove bin

--- provider-api/bin/main/com/opentune/provider/CodecCapabilities.kt
+++
@@ -1,8 +0,0 @@
-package com.opentune.provider
-
-data class CodecCapabilities(
-    val supportedVideoMimeTypes: List<String>,
-    val supportedAudioMimeTypes: List<String>,
-    val maxVideoPixels: Int = 1920 * 1080,
-    val supportedSubtitleFormats: List<String> = listOf("srt", "ass", "ssa", "vtt"),
-)
