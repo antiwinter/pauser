209631aed71c897e5a161127b0957e487515c74d
antiwinter <antiwinter@126.com>
2026-06-05 20:46:41 +0800

    add a tool [no ci]

---
+++ windump.bat
@@ -0,0 +1,4 @@
+@echo off
+mkdir dump
+adb -s 25b41579 exec-out "run-as com.opentune.app sh -c 'cd cache && tar cz emby_dump'" > emby_dump.tar.gz && tar xzf emby_dump.tar.gz -C dump
+del emby_dump.tar.gz
