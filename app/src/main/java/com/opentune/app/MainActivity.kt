d849945dd01b2dd82af62483326ba176f276710f
antiwinter <antiwinter@126.com>
2026-07-04 14:31:41 +0800

    change to dark mode

--- app/src/main/java/com/opentune/app/MainActivity.kt
+++ app/src/main/java/com/opentune/app/MainActivity.kt
@@ -4,16 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.opentune.app.navigation.OpenTuneNavHost
+import com.opentune.app.ui.theme.OpenTuneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            OpenTuneTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     OpenTuneNavHost()
                 }
